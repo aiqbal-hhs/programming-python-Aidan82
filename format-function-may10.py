29e98cf00a7c6e23467aea80c2ac98f4e4d93d19
Aidan82 <80796959+Aidan82@users.noreply.github.com>
2021-05-31 10:45:29 +1200

    Delete format-function-may10.py

--- format-function-may10.py
+++
@@ -1,9 +0,0 @@
-name = input("What is your name?")
-age = input("How old are you?")
-nationality = input("What is your nantionality?")
-dob = input("What is your date of birth?")
-color = input("What is your favourite color?")
-born = input("Where were you born")
-career = input("What is your job?")
-
-print("Hi my name is {}, I am the {}. My home place is {} and i am currently {}. My nationality is {} and i my favourite color is {}!".format(name, career, born, age, nationality, color))
