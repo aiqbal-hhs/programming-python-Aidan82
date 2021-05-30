111a3adf8b2ebc4e05cdcbb091469051c049f202
Aidan82 <80796959+Aidan82@users.noreply.github.com>
2021-05-31 10:46:10 +1200

    Add files via upload
    Comments

---
+++ Comments.py
@@ -0,0 +1,12 @@
+#asks the user there age and name.
+
+your_name = input("What's you name?")
+your_age = int(input("How old are you?"))
+
+#will calculate how old the user will be in 5 years time.
+
+age_in_5 = your_age + 5
+
+#tells the user the final print statement
+
+print("Well {}, in five years time you will be {}".format(your_name,age_in_5))
