0b9b5596f9528ea5ff6e848a7868c0a425fd5bd0
Aidan82 <80796959+Aidan82@users.noreply.github.com>
2021-06-03 14:47:52 +1200

    Add files via upload
    else and If

---
+++ el se and i f.py
@@ -0,0 +1,12 @@
+num_b = int(input("What number should b be?"))
+num_a = int(input("What number should a be?"))
+
+if num_b > num_a:
+    print("b is greater than a")
+
+else: 
+    print("a is greater than b")
+
+if num_a == num_b:
+    print("a is equal to b")
+
