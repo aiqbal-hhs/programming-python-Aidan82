d2d68286abea9c516f019d036c389e1fb0588629
Aidan82 <80796959+Aidan82@users.noreply.github.com>
2021-07-08 14:41:46 +1200

    Delete Printinput.py

--- Printinput.py
+++
@@ -1,7 +0,0 @@
-name = input("What is your name?")
-
-age = input("What is your age?")
-
-address = input("Where do you live?")
-
-print("I am {} and I am {} years old and I live at {}.".format(name, age, address))
